3ca103f49a7810a8b369e94d70e2fc2d63eefe3a
Ronnald R. Machado <rrmachado@id.uff.br>
2017-07-05 16:51:31 -0300

    Improvements in admin page

--- admin.py
+++ admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Record
 
-admin.site.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('title','created','updated','trecord','category')#Tabela Principal
+    list_filter = ('created','updated','category')#Tabela dos Filtros
+    search_fields = ('title', 'body')#Não está funcionando corretamente
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Record, RecordAdmin)
